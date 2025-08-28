31cb2e493a385317c2f4001336d44ee47541c492
Prigid Collection <prigidcollection@gmail.com>
2025-08-28 22:22:55 +0000

    When sign-in or sign up the is an error firebase auth config not found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "skyhost-z740f",
-  "appId": "1:239758760959:web:90010286d9cc2783a865ca",
-  "storageBucket": "skyhost-z740f.firebasestorage.app",
-  "apiKey": "AIzaSyDi8iiptVqGFhc0bTfoXUOiSjDXSUUfico",
-  "authDomain": "skyhost-z740f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "239758760959"
+  apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
