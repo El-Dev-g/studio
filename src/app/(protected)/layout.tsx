7a1b637ceae850df38517f24a12e9868232ee6eb
Prigid Collection <prigidcollection@gmail.com>
2025-08-28 18:10:29 +0000

    Log in button not working

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 // This is a simulated authentication check.
 // In a real application, you'd check for a valid session or token.
-const isAuthenticated = false;
+const isAuthenticated = true;
 
 export default function ProtectedLayout({
   children,
